b96f5f0fc0d1ef2e84b0eb58afc3ce95dd3bdc2d
far1shs <heliuliua@outlook.com>
2024-11-04 13:29:54 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="margin: -8px">
+    <Menubar :model="items">
+      <template #start>
+        <h4 style="margin: 0">Simple Cloud</h4>
+      </template>
+      <template #end>
+        <Button icon="pi pi-cog" text size="small"/>
+      </template>
+    </Menubar>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const items = ref([
+  {
+    label: "服务器"
+  }, {
+    label: "域名"
+  }
+]);
+</script>
